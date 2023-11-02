14d27d16e39ddf0b29f0db28a5a3260d306311bc
yuriy.poprishin <poprigushin@mail.ru>
2023-11-02 17:16:34 +0300

    Limits fixing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,10 @@
 enum class CardType {Unknown, Mastercard, Maestro, Visa, Mir, VK_Pay}
 //---------------------------------------------------------------------
 
+const val TRANSFER_LIMIT_ERROR : Int = -1
+const val MONTH_LIMIT_ERROR : Int = -2
+//---------------------------------------------------------------------
+
 fun calsTransferTax(sum : Int, totalMonthSum : Int = 0, cardTypeFrom : CardType = CardType.VK_Pay) : Int {
     var result : Int = 0;
 
@@ -11,11 +15,35 @@ fun calsTransferTax(sum : Int, totalMonthSum : Int = 0, cardTypeFrom : CardType 
     val tax_percent_Visa_Mir : Double = 0.75
     val tax_fixed_Visa_Mir : Int = 35
 
+    val monthLimit_Card : Int = 600_000
+    val transferLimit_Card : Int = 150_000
+
+    val monthLimit_VK_Pay : Int = 600_000
+    val transferLimit_VK_Pay : Int = 150_000
+
     var tax_percent : Double = 0.0
     var tax_fixed : Int = 0
+    var monthLimit : Int = 0
+    var transferLimit : Int = 0
+
+    if (cardTypeFrom == CardType.VK_Pay) {
+        monthLimit = monthLimit_VK_Pay;
+        transferLimit = transferLimit_VK_Pay;
+    }
+    else {
+        monthLimit = monthLimit_Card;
+        transferLimit = transferLimit_Card;
+    }
+
+    if (sum > transferLimit) {
+        return TRANSFER_LIMIT_ERROR;
+    }
+    if (sum + totalMonthSum > monthLimit) {
+        return MONTH_LIMIT_ERROR;
+    }
 
     if (cardTypeFrom == CardType.Mastercard || cardTypeFrom == CardType.Maestro) {
-        if (totalMonthSum > monthLimit_Master_Maestro) {
+        if (sum + totalMonthSum > monthLimit_Master_Maestro) {
             tax_percent  = tax_percent_Master_Maestro
             tax_fixed = tax_fixed_Master_Maestro
         }
